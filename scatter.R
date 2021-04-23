a976be9d1d5a9d8783210b50902925f240e2fe60
fredenck <fredzhang2013@gmail.com>
2021-04-23 15:57:15 -0700

    revised graph + code

--- scatter.R
+++ scatter.R
@@ -1,7 +1,7 @@
-colorsConv <- function(pos) {
+colorsConv <- function(arr) {
   color <- vector(mode = "character", length = length(pos))
-  for (i in 1:length(pos)) {
-    if (pos[i] == 1) {
+  for (i in 1:length(arr)) {
+    if (arr[i] == 1) {
       color[i] = "red"
     }else {
       color[i] = "blue"
@@ -15,16 +15,17 @@ windows(width = 12, height = 10)
 hug <- read.csv("hug.csv")
 age <- hug$age
 igg <- hug$IgG_Ratio
+# pos <- hug$Sex
 pos <- hug$pos
 color = colorsConv(pos)
 
 # age <- scan("age.txt")
 # igg <- scan("igg.txt")
 
-plot(age, igg, col=color, main="Age, IgG, and Sex",
+plot(age, igg, col=color, main="COVID Based on Age and IgG Levels",
      xlab="Age (years)", ylab="IgG Ratios", pch=19)
 
-legend(x="topright", legend = c("Positive", "Negative"), col=c("red","blue"), pch=1)
+legend(x="topright", legend = c("Positive", "Negative"), col=c("red","blue"), pch=19)
 
 # abline(lm(igg~age), col="red") # regression line (y~x)
 # lines(lowess(age,igg), col="blue") # lowess line (x,y)
